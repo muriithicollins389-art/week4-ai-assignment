d74df4c1814830f60b1435c55499ff8a8f6898c6
muriithicollins389-art <muriithicollins389@gmail.com>
2026-03-13 00:04:54 +0300

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,22 @@
+# Question 4
+# print("Hello AI Student")
+
+#Question 5
+name = "Danlex"
+age = 30
+city = "Nairobi"
+print("Name:", Danlex)
+print("Age:", 30)
+print("City:", Nairobi)
+# QUestion 6
+ languages = ["Java", "C++","Python","JavaScript", "Go"]
+print(languages[0])
+Java
+### Question 7: Dictionary of Student Information
+student = {"Name": "Danlex",
+    "Age": 30,
+    "Course": "AI"}
+
+print(student["Name"])
+Danlex
+  a list is an ordered sequence accessed by a numerical index, while a dictionary is a collection of unique key-value pairs accessed by a specific key
